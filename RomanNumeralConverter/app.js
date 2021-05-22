2e34a9f8f8b7835bcb7fafddb1df0d3660b1f27f
Joshua Godfrey <Joshua.godfrey@proplogix.com>
2021-05-22 16:46:56 -0400

    Feature: finished the roman numeral converter project.

--- RomanNumeralConverter/app.js
+++ RomanNumeralConverter/app.js
@@ -1,44 +1,32 @@
+
 /**
- * Roman Numeral Converter
+ * @author Joshua Godfrey <Godfreyjosh.jg@gmail.com>
  *
  * This program will convert an integer into a roman numeral
  * all roman numeral answers will be returned in upper case
  *
+ * @param num {number} the number the will convert into a roman numeral
+ *
+ * @function convertToRoman
  * */
 
-// var romanNumerals = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']; //an array of roman numerals in order from largest to smallest
-// var decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]; //an array of decimals values that match the index of the roman numerals
+function convertToRoman(num) {
+    let romanNumeralObj = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
+    // create an object to hold the roman numeral table. Each key value pair will hold the numeral and digit
+    let convertedString = '';
+    // iterate through the roman numeral object
 
-console.log("script running! ")
-
-var romanArrayObj = [
-    { d:1, r:'I' },
-    { d:4, r:'IV' },
-    { d:5, r:'V' },
-    { d:9, r:'IX' },
-    { d:10, r:'X' },
-    { d:40, r:'XL' },
-    { d:50, r:'L' },
-    { d:90, r:'XC' },
-    { d:100, r:'C' },
-    { d:400, r:'CD' },
-    { d:500, r:'D' },
-    { d:900, r:'CM' },
-    { d:1000, r:'M' }
-];
-
-let romanNum = parseInt(prompt("Enter a number to convert!"));
-
-function convertToRoman(romanNum) {
-    var roman = '';
-
-    if (romanNum > 0) {
-
-    } else {
-        alert("This isn't a valid number")
+    for ( let i in romanNumeralObj ) {
+        // console.log(`roman numeral: ${i} === ${romanNumeralObj[i]}`);
+        // if the number passed in is larger, or equal to the integer,
+        while ( num >= romanNumeralObj[i] ) {
+            // push the roman numeral to the empty string
+            convertedString += i;
+            // subtract from the orginal number passed
+            num -= romanNumeralObj[i];
+        }
     }
-
-    return roman;
+    console.log(convertedString)
+    return convertedString;
 }
 
-convertToRoman(36);
