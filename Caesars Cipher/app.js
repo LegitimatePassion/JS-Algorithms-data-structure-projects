4b9a6f479d31e144caca8e628282953830a22a16
Joshua Godfrey <Joshua.godfrey@proplogix.com>
2021-05-26 19:08:31 -0400

    Feature: started building the caesars cipher project in my js certification with free code camp

--- Caesars Cipher/app.js
+++ Caesars Cipher/app.js
@@ -2,74 +2,57 @@
  * Author Joshua Godfrey <Godfreyjosh.jg@gmail.com>
  *
  *
- * {param}
+ * @param {string} str
  *
  *
  * */
 
-
 function rot13(str) {
-    let cipher = {
-         A:'N',
-        'B':'O',
-        'C':'P',
-        'D':'Q',
-        'E':'R',
-        'F':'S',
-        'G':'T',
-        'H':'U',
-        'I':'V',
-        'J':'W',
-        'K':'X',
-        'L':'Y',
-        'M':'Z',
-        'N':'A',
-        'O':'B',
-        'P':'C',
-        'Q':'D',
-        'R':'E',
-        'S':'F',
-        'T':'G',
-        'U':'H',
-        'V':'I',
-        'W':'J',
-        'X':'K',
-        'Y':'L',
-        'Z':'M',};
-    var brokenCipher = '';
-    var wordArr = [...str];
-    // console.log(wordArr)
-    var myRegex = /[a-z]/i
-
-    // if the typeof the string does not equal string, stop the function
-    if (typeof str !== "string") {
-        console.log('This is not a string')
-        return false
-        // if the value passed in is a string, loop through each letter of the word and replace it with the value mapped to the key (from the cipher
-    } else {
-        // todo there is something broken with this loop, it's returning an empty string
-        for (let i = 0; i < wordArr; i++) {
-            if (wordArr[i] === ' ') {
-                brokenCipher += ' ';
-                console.log(brokenCipher)
-                return
-            } else if (!wordArr[i].match(myRegex)) {
-                brokenCipher += wordArr[i]
-                return
-            } else {
-                brokenCipher += wordArr[i]
-                return
-            }
-        }
+    let stringArg = str;
+    var cipher = {
+        A: 'N',
+        B: 'O',
+        C: 'P',
+        D: 'Q',
+        E: 'R',
+        F: 'S',
+        G: 'T',
+        H: 'U',
+        I: 'V',
+        J: 'W',
+        K: 'X',
+        L: 'Y',
+        M: 'Z',
+        N: 'A',
+        O: 'B',
+        P: 'C',
+        Q: 'D',
+        R: 'E',
+        S: 'F',
+        T: 'G',
+        U: 'H',
+        V: 'I',
+        W: 'J',
+        X: 'K',
+        Y: 'L',
+        Z: 'M',
+    };
+    let brokenCipher = '';
+    let wordArr = [];
+    for (const letter of str) {
+        wordArr.push(letter);
     }
+    console.log(wordArr)
+    // console.log(wordArr)
+    // if the typeof the string does not equal string, stop the function
+    // if (typeof str !== "string") {
+    //     console.log('This is not a string')
+    //     return false
 
 
-    console.log(brokenCipher)
+    console.log(brokenCipher);
     return brokenCipher;
 }
 
-let testString = "SERR PBQR PNZC";
+rot13('HELLO HI JH')
 
-let result = typeof testString;
-
-rot13();
