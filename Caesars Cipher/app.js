cd25ce9ea2c724355ec80f36bccecf4808953c96
Joshua Godfrey <Joshua.godfrey@proplogix.com>
2021-05-30 14:37:15 -0400

    Feature: Got the decoder working.

--- Caesars Cipher/app.js
+++ Caesars Cipher/app.js
@@ -1,58 +1,44 @@
 /**
- * Author Joshua Godfrey <Godfreyjosh.jg@gmail.com>
+ * (Author) Joshua Godfrey <Godfreyjosh.jg@gmail.com>
  *
- *
- * @param {string} str
+ * (Function rot13) - This function will take any ROT13 encoded string and return a decoded string.
+ * @param {string} str the string we need to decipher.
  *
  *
  * */
 
 function rot13(str) {
-    let stringArg = str;
-    var cipher = {
-        A: 'N',
-        B: 'O',
-        C: 'P',
-        D: 'Q',
-        E: 'R',
-        F: 'S',
-        G: 'T',
-        H: 'U',
-        I: 'V',
-        J: 'W',
-        K: 'X',
-        L: 'Y',
-        M: 'Z',
-        N: 'A',
-        O: 'B',
-        P: 'C',
-        Q: 'D',
-        R: 'E',
-        S: 'F',
-        T: 'G',
-        U: 'H',
-        V: 'I',
-        W: 'J',
-        X: 'K',
-        Y: 'L',
-        Z: 'M',
-    };
-    let brokenCipher = '';
-    let wordArr = [];
-    for (const letter of str) {
-        wordArr.push(letter);
+    let alphabet13 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let alphabetArray = [...alphabet13];
+    // console.log(alphabetArray)
+    let cipher13 = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+    // let cipherArray = [...cipher13];
+    // console.log(cipherArray)
+    // the empty array we will use to store the deciphered phrase
+    let brokenCipher = [];
+
+    for (let i = 0; i < str.length; i++) {
+        let stringIndex = alphabetArray.indexOf(str[i]);
+        if (cipher13.charCodeAt(stringIndex) > 0) {
+            brokenCipher.push(String.fromCharCode(cipher13.charCodeAt(stringIndex)));
+        } else {
+            brokenCipher.push(str[i])
+        }
     }
-    console.log(wordArr)
-    // console.log(wordArr)
-    // if the typeof the string does not equal string, stop the function
-    // if (typeof str !== "string") {
-    //     console.log('This is not a string')
-    //     return false
-
-
-    console.log(brokenCipher);
-    return brokenCipher;
+    let newString = brokenCipher.join('').toUpperCase();
+    console.log(newString);
+    return newString;
 }
 
-rot13('HELLO HI JH')
+let broken = rot13('HELLO HI JH!');
+console.log(broken);
+let broken1 = rot13("SERR PBQR PNZC");
+console.log(broken1);
+let broken2 = rot13("SERR CVMMN!");
+console.log(broken2);
+let broken3 = rot13("SERR YbiR?");
+console.log(broken3);
+let broken4 = rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.");
+console.log(broken4);
+
 
